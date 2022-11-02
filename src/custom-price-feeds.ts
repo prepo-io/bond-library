abe375bca9bed03fe907a80f332560b2015c66fc
chrisling-dev <chrisling.dev@gmail.com>
2022-11-02 16:26:36 +0800

    feat: update links and network

--- src/custom-price-feeds.ts
+++ src/custom-price-feeds.ts
@@ -1,29 +1,34 @@
-import { CHAIN_ID } from "../constants";
-import CUSTOM_PRICE_FEEDS from "../custom-price-feeds";
-import { ProtocolDefinition } from "../types";
+import { ethers } from "ethers";
+import { formatUnits } from "ethers/lib/utils";
+import { CHAIN_ID } from "./constants";
+import { CustomPriceFunction } from "./types";
 
+//add a cool name to your custom price fetcher
+export enum CUSTOM_PRICE_FEEDS {
+  US_STABLE = "us_stable",
+  PPO = "PPO",
+}
+
+//show your cool name off below along with your coding skills
 export default {
-  name: "prePO",
-  description:
-    "prePO is a decentralized trading platform allowing anyone, anywhere to gain exposure to any pre-public asset.",
-  logoUrl: "https://imgur.com/nDauUpK",
-  links: {
-    governanceVote: "https://snapshot.org/#/prepo.eth",
-    twitter: "https://twitter.com/prepo_io",
-    github: "https://github.com/prepo-io",
-    medium: "https://medium.com/prepo",
-    homepage: "https://prepo.io/",
-    discord: "https://discord.com/invite/XSQRdJactS",
+  [CUSTOM_PRICE_FEEDS.US_STABLE]: async () => 1.0,
+  [CUSTOM_PRICE_FEEDS.PPO]: async () => {
+    const ORACLE_ABI = ["function getFixedPrice() view returns (uint256)"];
+    const ORACLE_ADDRESS = "0x503aef858e26C136C29e40483EA0297160d2BDC0";
+    const PRICE_DECIMALS = 6;
+
+    try {
+      const defaultProvider = ethers.getDefaultProvider(CHAIN_ID.ARBITRUM_MAINNET);
+
+      const fixedPriceContract = new ethers.Contract(ORACLE_ADDRESS, ORACLE_ABI, defaultProvider);
+
+      const priceBN = await fixedPriceContract.getFixedPrice();
+
+      if (priceBN) return +formatUnits(priceBN, PRICE_DECIMALS);
+
+      throw new Error();
+    } catch (e) {
+      throw new Error("Error getting custom price for PPO token.");
+    }
   },
-  issuerAddresses: { [CHAIN_ID.GOERLI_TESTNET]: "0xe5011a7cc5CDA29F02CE341B2847B58abEFA7c26" },
-  tokens: [
-    {
-      name: "prePO",
-      symbol: "PPO",
-      logoUrl: "https://imgur.com/NNI5hnR",
-      priceSources: [{ source: "custom", customPriceFunction: CUSTOM_PRICE_FEEDS.PPO }],
-      purchaseLinks: { [CHAIN_ID.GOERLI_TESTNET]: "https://sale.prepo.io/" },
-      addresses: { [CHAIN_ID.GOERLI_TESTNET]: "0xB40DBBb7931Cfef8Be73AEEC6c67d3809bD4600B" },
-    },
-  ],
-} as ProtocolDefinition;
+} as { [key: string]: CustomPriceFunction };
--- src/protocols/prepo.ts
+++ src/protocols/prepo.ts
@@ -4,26 +4,26 @@ import { ProtocolDefinition } from "../types";
 
 export default {
   name: "prePO",
-  description:
-    "prePO is a decentralized trading platform allowing anyone, anywhere to gain exposure to any pre-public asset.",
-  logoUrl: "https://imgur.com/nDauUpK",
+  description: "The Decentralized Exchange for Pre-IPO Stocks & Pre-IDO Tokens.",
+  logoUrl: "https://i.imgur.com/NNI5hnR.png",
   links: {
     governanceVote: "https://snapshot.org/#/prepo.eth",
-    twitter: "https://twitter.com/prepo_io",
-    github: "https://github.com/prepo-io",
-    medium: "https://medium.com/prepo",
     homepage: "https://prepo.io/",
-    discord: "https://discord.com/invite/XSQRdJactS",
+    discord: "https://discord.gg/prePO",
+    twitter: "https://twitter.com/prepo_io",
+    medium: "https://medium.com/prepo",
+    telegram: "https://t.me/prePO_News",
+    github: "https://github.com/prepo-io",
   },
-  issuerAddresses: { [CHAIN_ID.GOERLI_TESTNET]: "0xe5011a7cc5CDA29F02CE341B2847B58abEFA7c26" },
+  issuerAddresses: { [CHAIN_ID.ARBITRUM_MAINNET]: "0xe5011a7cc5CDA29F02CE341B2847B58abEFA7c26" },
   tokens: [
     {
-      name: "prePO",
+      name: "prePO Token",
       symbol: "PPO",
       logoUrl: "https://imgur.com/NNI5hnR",
       priceSources: [{ source: "custom", customPriceFunction: CUSTOM_PRICE_FEEDS.PPO }],
-      purchaseLinks: { [CHAIN_ID.GOERLI_TESTNET]: "https://sale.prepo.io/" },
-      addresses: { [CHAIN_ID.GOERLI_TESTNET]: "0xB40DBBb7931Cfef8Be73AEEC6c67d3809bD4600B" },
+      purchaseLinks: { [CHAIN_ID.ARBITRUM_MAINNET]: "https://sale.prepo.io/" },
+      addresses: { [CHAIN_ID.ARBITRUM_MAINNET]: "0xB40DBBb7931Cfef8Be73AEEC6c67d3809bD4600B" },
     },
   ],
 } as ProtocolDefinition;
